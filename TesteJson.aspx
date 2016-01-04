9f7b682bd5c8c62e4d0137aaf3e60af2b93044e1
henriquesm <henriques.m@terra.com.br>
2016-01-04 15:03:41 -0200

    Client REST
    Consumo de REST usando HttpWebRequest

---
+++ TesteJson.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TesteJson.aspx.cs" Inherits="TestClientJson.TesteJson" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Panel ID="PnlUrl" runat="server" Style="width: 100%">
+                <asp:RadioButtonList ID="rdMetodoEnvio" runat="server" OnSelectedIndexChanged="rdMetodoEnvio_SelectedIndexChanged" AutoPostBack="true">
+                    <asp:ListItem Text="Get" Value="Get" Selected="True"></asp:ListItem>
+                    <asp:ListItem Text="Post" Value="Post"></asp:ListItem>
+                </asp:RadioButtonList>
+                <asp:Label ID="lblUrl" runat="server" Text="URL:"></asp:Label>
+                <asp:TextBox ID="txtUrl" runat="server" Width="450px"></asp:TextBox>
+                <hr />
+            </asp:Panel>
+            <asp:Panel ID="PnlPost" runat="server">
+                <asp:Label ID="lbParametrosPost" runat="server" Text="Parametros POST:"></asp:Label>
+                <br />
+                <asp:TextBox ID="txtParametrosPost" runat="server" TextMode="MultiLine" Rows="10" Columns="100">
+
+                </asp:TextBox>
+                <hr />
+            </asp:Panel>
+            <asp:Panel ID="PnlBotao" runat="server" Style="width: 100%">
+                <asp:Button ID="btnExecutar" runat="server" Text="Consultar" OnClick="btnExecutar_Click" />
+                <hr />
+            </asp:Panel>
+
+            <asp:Panel ID="PnlRetorno" runat="server" Style="width: 100%">
+                <asp:Label ID="lbRetorno" runat="server" Text="Retorno:"></asp:Label><br />
+                <asp:TextBox ID="txtRetorno" runat="server" TextMode="MultiLine" Rows="10" Columns="100"></asp:TextBox>
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TesteJson.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestClientJson
+{
+    public partial class TesteJson : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (rdMetodoEnvio.SelectedIndex > 0)
+            {
+                PnlPost.Visible = (rdMetodoEnvio.SelectedValue == "Post");
+            }
+            else
+            {
+                PnlPost.Visible = false;
+            }
+
+            if (!IsPostBack)
+            {
+            }
+        }
+
+        protected void btnExecutar_Click(object sender, EventArgs e)
+        {
+            string url = string.Empty;
+
+            if (!string.IsNullOrEmpty(txtUrl.Text))
+            {
+                url = txtUrl.Text;
+                var request = System.Net.HttpWebRequest.Create(url);
+
+                if (PnlPost.Visible)
+                {
+                    request.Method = "POST";
+                    string postData = txtParametrosPost.Text;
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                    request.ContentType = "application/json";
+                    request.ContentLength = byteArray.Length;
+                    Stream dataStream = request.GetRequestStream();
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Close();
+                }
+                else
+                    request.Method = "GET";
+
+                var response = request.GetResponse();
+                var streamReader = new System.IO.StreamReader(response.GetResponseStream());
+                var json = streamReader.ReadToEnd();
+
+                streamReader.Close();
+                response.Close();
+
+                if (json != null)
+                    txtRetorno.Text = json.ToString();
+            }
+            else
+                txtRetorno.Text = "URL Invalida.";
+
+
+        }
+
+        protected void rdMetodoEnvio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
